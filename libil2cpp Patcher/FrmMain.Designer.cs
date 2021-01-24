4d5a4aca27e953e6148833dabaa77804168fe7fb
TFive <t5_@outlook.co.th>
2021-01-24 15:06:11 +0700

    Initial commit

---
+++ libil2cpp Patcher/FrmMain.Designer.cs
@@ -0,0 +1,288 @@
+﻿
+namespace libil2cpp_Patcher
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFile = new System.Windows.Forms.Button();
+            this.txtFile = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnPatch = new System.Windows.Forms.Button();
+            this.dataList = new System.Windows.Forms.DataGridView();
+            this.CheatName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Offset = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Byte = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.cbHelper = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataList)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(419, 22);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(74, 23);
+            this.btnFile.TabIndex = 20;
+            this.btnFile.Text = "Select";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // txtFile
+            // 
+            this.txtFile.Location = new System.Drawing.Point(98, 22);
+            this.txtFile.Name = "txtFile";
+            this.txtFile.Size = new System.Drawing.Size(315, 23);
+            this.txtFile.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Executable file:";
+            // 
+            // btnPatch
+            // 
+            this.btnPatch.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.btnPatch.Location = new System.Drawing.Point(5, 398);
+            this.btnPatch.Name = "btnPatch";
+            this.btnPatch.Size = new System.Drawing.Size(489, 45);
+            this.btnPatch.TabIndex = 23;
+            this.btnPatch.Text = "Patch";
+            this.btnPatch.UseVisualStyleBackColor = true;
+            this.btnPatch.Click += new System.EventHandler(this.btnPatch_Click);
+            // 
+            // dataList
+            // 
+            this.dataList.AllowUserToResizeColumns = false;
+            this.dataList.AllowUserToResizeRows = false;
+            this.dataList.BackgroundColor = System.Drawing.Color.White;
+            this.dataList.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            this.dataList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CheatName,
+            this.Offset,
+            this.Byte});
+            this.dataList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataList.Location = new System.Drawing.Point(5, 21);
+            this.dataList.Name = "dataList";
+            this.dataList.RowHeadersVisible = false;
+            this.dataList.RowTemplate.Height = 25;
+            this.dataList.Size = new System.Drawing.Size(489, 377);
+            this.dataList.TabIndex = 24;
+            this.dataList.KeyDown += new System.Windows.Forms.KeyEventHandler(this.dataList_KeyDown);
+            // 
+            // CheatName
+            // 
+            this.CheatName.HeaderText = "Name";
+            this.CheatName.Name = "CheatName";
+            this.CheatName.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Offset
+            // 
+            this.Offset.HeaderText = "Offset";
+            this.Offset.Name = "Offset";
+            this.Offset.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Offset.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Byte
+            // 
+            this.Byte.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Byte.HeaderText = "Byte";
+            this.Byte.Name = "Byte";
+            this.Byte.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Programmatic;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataList);
+            this.groupBox1.Controls.Add(this.btnPatch);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(5, 96);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(499, 448);
+            this.groupBox1.TabIndex = 25;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Patcher";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(6, 51);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 26;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(87, 51);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 27;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(249, 51);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 28;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(168, 51);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 29;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // cbHelper
+            // 
+            this.cbHelper.FormattingEnabled = true;
+            this.cbHelper.Items.AddRange(new object[] {
+            "return true",
+            "return false",
+            "return 0"});
+            this.cbHelper.Location = new System.Drawing.Point(381, 52);
+            this.cbHelper.Name = "cbHelper";
+            this.cbHelper.Size = new System.Drawing.Size(112, 23);
+            this.cbHelper.TabIndex = 30;
+            this.cbHelper.SelectedIndexChanged += new System.EventHandler(this.cbHelper_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(330, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 15);
+            this.label2.TabIndex = 31;
+            this.label2.Text = "&Helper:";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "XML|*.xml|All files|*.*";
+            this.saveFileDialog1.Title = "Save your settings";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "XML|*.xml|All files|*.*";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.btnFile);
+            this.groupBox2.Controls.Add(this.txtFile);
+            this.groupBox2.Controls.Add(this.btnLoad);
+            this.groupBox2.Controls.Add(this.cbHelper);
+            this.groupBox2.Controls.Add(this.btnRemove);
+            this.groupBox2.Controls.Add(this.btnSave);
+            this.groupBox2.Controls.Add(this.btnClear);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(5, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(499, 91);
+            this.groupBox2.TabIndex = 32;
+            this.groupBox2.TabStop = false;
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.DefaultExt = "libil2cpp.so";
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Filter = "SO|*.so|All files|*.*";
+            // 
+            // FrmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 549);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FrmMain";
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lib IL2CPP Patcher";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.FrmMain_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.FrmMain_DragEnter);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.FrmMain_DragOver);
+            ((System.ComponentModel.ISupportInitialize)(this.dataList)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.TextBox txtFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPatch;
+        private System.Windows.Forms.DataGridView dataList;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.ComboBox cbHelper;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CheatName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Offset;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Byte;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+    }
+}
+
---
+++ libil2cpp Patcher/FrmMain.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Data;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace libil2cpp_Patcher
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        #region Drag/Drop
+
+        private void FrmMain_DragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var file in files)
+            {
+                Path.GetExtension(file);
+                e.Effect = DragDropEffects.Copy;
+            }
+        }
+
+        private void FrmMain_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                if (!e.Data.GetDataPresent(DataFormats.FileDrop)) return;
+                var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+                txtFile.Text = files[0];
+            }
+            catch
+            {
+            }
+        }
+
+        private void FrmMain_DragOver(object sender, DragEventArgs e)
+        {
+            var files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (var file in files)
+            {
+                Path.GetExtension(file);
+            }
+        }
+
+        #endregion Drag/Drop
+
+        #region Settings
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            txtFile.Text = openFileDialog1.FileName;
+        }
+
+        #region Remove/Clear
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            RemoveRows();
+        }
+
+        private void dataList_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                RemoveRows();
+            }
+        }
+
+        private void RemoveRows()
+        {
+            try
+            {
+                if (dataList.RowCount <= 1 || dataList.CurrentCell.RowIndex == dataList.RowCount - 1) return;
+                dataList.Rows.RemoveAt(dataList.SelectedCells[0].RowIndex);
+            }
+            catch
+            {
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                while (dataList.RowCount > 1)
+                {
+                    dataList.Rows.RemoveAt(0);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        #endregion Remove/Clear
+
+        private void cbHelper_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbHelper.SelectedIndex)
+            {
+                case 0:
+                    BeginInvoke(new Action(() => cbHelper.Text = @"01 00 A0 E3 1E FF 2F E1"));
+                    break;
+
+                case 1:
+                case 2:
+                    BeginInvoke(new Action(() => cbHelper.Text = @"00 00 A0 E3 1E FF 2F E1"));
+                    break;
+            }
+        }
+
+        #region Load/Save Xml
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void LoadData()
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK) return;
+            dataList.DataSource = null;
+            dataList.Columns.Clear();
+            try
+            {
+                var path = openFileDialog1.FileName;
+                var dataSet = new DataSet();
+                dataSet.ReadXml(path);
+                dataList.DataSource = dataSet.Tables[0];
+                dataList.Columns[2].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            }
+            catch
+            {
+            }
+        }
+
+        //https://stackoverflow.com/a/26335800/8902883
+        private void SaveData()
+        {
+            if (saveFileDialog1.ShowDialog() != DialogResult.OK) return;
+            var path = saveFileDialog1.FileName;
+
+            var dt = new DataTable { TableName = "Patcher" };
+            for (var i = 1; i < dataList.Columns.Count + 1; i++)
+            {
+                var column = new DataColumn(dataList.Columns[i - 1].HeaderText);
+                dt.Columns.Add(column);
+            }
+            var columnCount = dataList.Columns.Count;
+            for (var index = 0; index < dataList.Rows.Count - 1; index++)
+            {
+                var dr = dataList.Rows[index];
+                var dataRow = dt.NewRow();
+                for (var i = 0; i < columnCount; i++)
+                {
+                    dataRow[i] = dr.Cells[i].Value;
+                }
+
+                dt.Rows.Add(dataRow);
+            }
+
+            var ds = new DataSet();
+            ds.Tables.Add(dt);
+            var xmlSave = new XmlTextWriter(path, Encoding.UTF8);
+
+            ds.WriteXml(xmlSave);
+            xmlSave.Close();
+        }
+
+        #endregion Load/Save Xml
+
+        #endregion Settings
+
+        private void btnPatch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.Copy(txtFile.Text, txtFile.Text + ".bak");
+            }
+            catch 
+            {
+                
+            }
+
+            for (var i = 0; i < dataList.RowCount - 1; i++)
+            {
+                var bytes = StringToByteArray(DataValue(2, i));
+                var offset = Convert.ToInt32(DataValue(1, i), 16);
+                using var fs = new FileStream(txtFile.Text, FileMode.Open, FileAccess.ReadWrite)
+                {
+                    Position = offset
+                };
+                fs.Write(bytes);
+            }
+        }
+
+        private string DataValue(int column, int row)
+        {
+            return dataList[column, row].Value.ToString()?.Replace(" ", "");
+        }
+
+        //https://stackoverflow.com/a/311179/8902883
+        private static byte[] StringToByteArray(String hex)
+        {
+            var numberChars = hex.Length;
+            var bytes = new byte[numberChars / 2];
+            for (var i = 0; i < numberChars; i += 2)
+                bytes[i / 2] = Convert.ToByte(hex.Substring(i, 2), 16);
+            return bytes;
+        }
+        private static string ByteArrayToString(byte[] ba)
+        {
+            return BitConverter.ToString(ba).Replace("-", "");
+        }
+    }
+}
